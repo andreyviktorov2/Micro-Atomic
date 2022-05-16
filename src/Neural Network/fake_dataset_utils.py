670ef7f05dbd1bd9b6581794191c97228a6f62a7
BATANOPE <44753309+BATANOPE@users.noreply.github.com>
2022-05-16 20:35:46 +0300

    Modified NN for fake dataset (#8)
    Co-authored-by: Andrey <test@gmail.com>

---
+++ src/Neural Network/fake_dataset_utils.py
@@ -0,0 +1,94 @@
+import os
+import sys
+import random
+import numpy as np
+
+class DatasetGenerator:
+
+  def __init__(self):
+    self.current_dir = os.path.dirname(sys.argv[0])
+    self.file_name_full = ""
+    self.image_width = 256
+    self.image_height = 20
+
+
+  def __CreateFile(self, file_name):
+    if not os.path.exists(os.path.join(self.current_dir, "datasets")):
+      os.mkdir(os.path.join(self.current_dir, "datasets"))
+
+    file_index = 1
+    while os.path.exists(os.path.join(                       
+        self.current_dir, "datasets", file_name +            
+        (str(file_index) if file_index > 1 else "") + ".txt")):
+      file_index += 1
+
+    file_name = file_name + (str(file_index) if file_index > 1 else "") + ".txt"
+    self.file_name_full = os.path.join(self.current_dir, "datasets", file_name)
+    open(self.file_name_full, 'w')
+    print("Created: " + self.file_name_full)
+
+
+  def __WriteMatrixToFile(self, file, matrix):    
+    for i in range(0, len(matrix)):
+      for j in range(0, len(matrix[0])):
+        file.write(str(matrix[i][j]) + ' ')
+      file.write("\n")
+
+
+  def GenerateDataset(self, file_name = "dataset", size = 100):
+    self.__CreateFile(file_name)
+
+    with open(self.file_name_full, 'w') as file:
+      file.write(str(size) + "\n")
+      file.write(str(self.image_width) + "\n")
+      file.write(str(self.image_height) + "\n")
+
+      for counter in range(0,size):
+        matrix = np.random.randint(50, 60, (self.image_height, self.image_width))
+        matrix = matrix / 100 # normalization
+
+        if counter >= size / 2:
+          triangle_width = np.random.randint(50,70)
+          triangle_height = np.random.randint(6, 8)
+          triangle_x0 = np.random.randint(0, self.image_width - triangle_width)
+          triangle_y0 = np.random.randint(0, self.image_height - triangle_height)
+          triangle_step = triangle_width / triangle_height / 2
+
+          step = 0.0
+          for i in range(triangle_y0, triangle_y0 + triangle_height):
+            for j in range(triangle_x0 + int(step), triangle_x0 + triangle_width - int(step)):
+              if triangle_x0 + step <= triangle_x0 + triangle_width - step:
+                matrix[i][j] = 1.0 # np.random.randint(950, 1001) / 1000
+            step += triangle_step
+        
+        self.__WriteMatrixToFile(file, matrix)
+
+
+def ReadDataset(path):
+  if os.path.exists(path):
+    with open(path, 'r') as file:
+      size = int(file.readline())
+      width = int(file.readline())
+      height = int(file.readline())
+      input = np.empty((size, height, width))
+      output = np.concatenate((np.zeros(int(size / 2)),
+                               np.ones(size - int(size / 2))))
+
+      for i in range(size):
+        for j in range(height):
+          input[i][j] = [float(number) for number in file.readline().split()]
+
+    print("Dataset " + path + " was successfully read")
+    return input, output, size, width, height
+  else:
+    print("File " + path + " does not exist")
+    return np.empty((1, 1, 1)), np.zeros(1), 0, 0, 0
+        
+
+
+
+# generator = DatasetGenerator().GenerateDataset(file_name="train", size=10000)
+# generator = DatasetGenerator().GenerateDataset(file_name="test", size=2000)
+
+# input, output, size, image_width, image_height = \
+#   ReadDataset("C:/UNN/Мага/ИТОПР/Micro-Atomic/src/Neural Network/datasets/test.txt")
--- src/Neural Network/nn_prototype.py
+++ src/Neural Network/nn_prototype.py
@@ -3,6 +3,7 @@ from tensorflow.keras import layers, optimizers, models
 import numpy as np
 import tensorflow as tf
 from data_parser import DataCollector
+from fake_dataset_utils import DatasetGenerator, ReadDataset
 
 import os
 import sys
@@ -12,60 +13,58 @@ def SaveModel(model,
     path = os.path.join(os.path.dirname(sys.argv[0]), "model.h5")):
   model.save(path)
 
+
 def PrintModelStructure(model,
     path = os.path.join(os.path.dirname(sys.argv[0]), "model.png")):
   print(model.summary())
   tf.keras.utils.plot_model(model, to_file = path, show_shapes = True)
 
+
 def CreateModel(image_x, image_y, learning_rate):
   model = models.Sequential()
 
   model.add(layers.Flatten(input_shape = (image_x, image_y)))
+  model.add(layers.Dense(units = 1024, activation = 'relu'))
+  model.add(layers.Dense(units = 512, activation = 'relu'))
   model.add(layers.Dense(units = 128, activation = 'relu'))
-  model.add(layers.Dense(units = 32, activation = 'relu'))
-  model.add(layers.Dense(units = 8, activation = 'relu'))
+  model.add(layers.Dense(units = 16, activation = 'relu'))
   model.add(layers.Dropout(rate = 0.2))
   model.add(layers.Dense(1, activation = 'sigmoid'))
 
-  model.compile(optimizer = optimizers.Adam(learning_rate = learning_rate),
-                                            loss = "sparse_categorical_crossentropy",
+  model.compile(optimizer = optimizers.SGD(learning_rate = learning_rate),
+                                            loss = "mean_squared_error",
                                             metrics = ['accuracy'])
   return model
 
 
 # collector to parse input data
-collector = DataCollector()
+# collector = DataCollector()
+# collector.ReadFile("C:/UNN/Мага/ИТОПР/Micro-Atomic/resources/proba.txt")
 
-# input parsed images - list of matrix (matrix = image)
-input_data = [[[ ]]]
-input_test_data = [[[ ]]]
-
-# 1 - defect; 0 - no defect
-target_data = 0
-target_test_data = 0
-
-image_x = 256
-image_y = 20
+# read fake dataset
+train_input, train_output, size, image_width, image_height = \
+  ReadDataset("C:/UNN/Maga/ITOPR/Micro-Atomic/src/Neural Network/datasets/train.txt")
+test_input, test_output, size, image_width, image_height = \
+  ReadDataset("C:/UNN/Maga/ITOPR/Micro-Atomic/src/Neural Network/datasets/test.txt")
 
 # model args
 learning_rate = 0.003
-epochs = 50
-batch_size = 4000
+epochs = 150
+batch_size = 300
 validation_split = 0.2
 
-
 # create model
-model = CreateModel(image_x, image_y, learning_rate)
-
-PrintModelStructure(model)
-SaveModel(model)
+model = CreateModel(image_height, image_width, learning_rate)
 
 # train model
-history = model.fit(x = input_data, y = target_data,
+history = model.fit(x = train_input, y = train_output,
                     batch_size = batch_size,
                     epochs = epochs, shuffle = True,
                     validation_split = validation_split)
 
 #test model
-model.evaluate(x = input_test_data, y = target_test_data,
+model.evaluate(x = test_input, y = test_output,
                batch_size = batch_size)
+
+# PrintModelStructure(model)
+# SaveModel(model)
