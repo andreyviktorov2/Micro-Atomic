0de672dc558add76e639296e417d1e78d3e1178a
andreyvi <test@gmail.com>
2022-06-26 18:23:14 +0300

    [FEATURE] Added prediction and data reading time

--- src/Neural Network/micro_atomic.py
+++ src/Neural Network/micro_atomic.py
@@ -16,13 +16,13 @@ if not os.path.exists(input_file):
   print("Input file doesn't exist")
   sys.exit(1)
 
-start_time = time.time()
-
 # open saved model
 model = load_model(os.path.join(os.path.dirname(sys.argv[0]), "model.h5"))
 
 # collector to parse input data
 collector = DataCollector()
+
+data_read_time = time.time()
 
 # read input_data
 if ".png" in input_file or ".jpeg" in input_file:
@@ -30,11 +30,15 @@ if ".png" in input_file or ".jpeg" in input_file:
 else:
   collector.ReadFile(input_file)
 
+data_read_time = time.time() - data_read_time
+
 # reshape input matrix
 input = np.empty(shape=(1, collector.ny, collector.nx))
 input[0] = collector.matrix / np.max(collector.matrix)
 
+prediction_time = time.time()
 prediction = model.predict(input)[0][0] * 100.0
+prediction_time = time.time() - prediction_time
 
 if "-f" in sys.argv:
   file_index = 1
@@ -51,11 +55,13 @@ if "-f" in sys.argv:
       file.write("Defect (probability " + "{:.2f}".format(prediction) + "%)\n")
     else:
       file.write("No defect (probability " + "{:.2f}".format(100 - prediction) + "%)\n")
-    file.write("Time: " + "{:.3f}".format(time.time() - start_time) + " seconds")
+    file.write("Data reading time: " + "{:.3f}".format(data_read_time) + " seconds\n")
+    file.write("Prediction time: " + "{:.3f}".format(prediction_time) + " seconds")
   print("Result was stored in " + file_name)
 else:
   if(prediction >= 50):
-    print("Defect (probability " + "{:.2f}".format(prediction) + "%)\n")
+    print("Defect (probability " + "{:.2f}".format(prediction) + "%)")
   else:
-    print("No defect (probability " + "{:.2f}".format(100 - prediction) + "%)\n")
-  print("Time: " + "{:.3f}".format(time.time() - start_time) + " seconds")
+    print("No defect (probability " + "{:.2f}".format(100 - prediction) + "%)")
+  print("Data reading time: " + "{:.3f}".format(data_read_time) + " seconds")
+  print("Prediction time: " + "{:.3f}".format(prediction_time) + " seconds")
