e83c16428b39a66f2fb97140c9809ff414ec826e
andreyvi <test@gmail.com>
2022-06-27 11:28:22 +0300

    Bug fix

--- src/Neural Network/nn_validation.py
+++ src/Neural Network/nn_validation.py
@@ -1,10 +1,15 @@
 from tensorflow.keras.models import load_model
 import os
+import sys
 from fake_dataset_utils import ReadDataset, ResultPrinter
 
-model = load_model(os.path.join("src", "Neural Network", "model.h5"))
+root = os.path.dirname(sys.argv[0])
+print()
+print(root)
+print()
+model = load_model(os.path.join(root, "model.h5"))
 
 test_input, test_output, size, image_width, image_height = \
-  ReadDataset(os.path.join("resources", "validation.txt"))
+  ReadDataset(os.path.join(root, "..", "..", "resources", "validation.txt"))
 
 ResultPrinter().PrintValidation(model, test_input, test_output)
